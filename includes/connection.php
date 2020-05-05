b56a8c856181195ae6c309e2534cb8e57f979936
Sendi Marvin <sendimarvin1@gmail.com>
2020-05-06 00:52:21 +0300

    Add Table data

---
+++ includes/connection.php
@@ -0,0 +1,2 @@
+<?php
+    $server = 'http://cresteddevelopers.com/AppFiles/SkulKitApp/manage-users.php?get_groups'
--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,38 @@
 
 
-$(document).ready(function() {
-    $('#example').DataTable({
-        "processing": true,
-        "serverSide": true,
-        "ajax": "scripts/server_processing.php",
-        "columns": [
-            { "data": "name" },
-            { "data": "position" },
-            { "data": "office" },
-            { "data": "extn" },
-            { "data": "start_date" },
-            { "data": "salary" }
-        ]
-    });
-} );
+$(function () {
+    // $('#example').DataTable({
+    //     "columns": [
+    //         { "data": "name" },
+    //         { "data": "group_status" },
+    //     ],
+    //     data:  [
+    //         {'name': 'Maths', 'group_status': 'Open'},
+    //         {'name': 'Maths', 'group_status': 'Open'},
+    //         {'name': 'Maths', 'group_status': 'Open'},
+    //         {'name': 'Maths', 'group_status': 'Open'},
+    //         {'name': 'Maths', 'group_status': 'Open'},
+    //     ]
+        
+    // });
+    getGroups();
+});
+
+function getGroups () {
+    $.get("http://cresteddevelopers.com/AppFiles/SkulKitApp/manage-users.php?get_groups", {}, 
+        data => {
+            console.log(data);
+            $('#example').DataTable({
+                "columns": [
+                    { "data": "name" },
+                    { "data": "description" },
+                    { "data": "level_name" },
+                    { "data": "sublect_name" },
+                    { "data": "first_name" },
+                    { "data": "group_status" },
+                ],
+                data:  data
+            });
+        }, 'JSON'
+    )
+}
